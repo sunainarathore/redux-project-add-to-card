4bbfe986a7493d7d9dc7e8e5dac050b53f70ae77
sunainarathore <51408352+sunainarathore@users.noreply.github.com>
2022-07-20 18:22:39 +0530

    add to card

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+import './App.css';
+import HeaderContainer from './containers/HeaderContainer';
+import HomeContainer from './containers/HomeContainer'
+function App() {
+ 
+  return (
+   <>
+     <HomeContainer/>
+     <HeaderContainer/>
+   </>
+  );
+}
+
+export default App;
+ 
